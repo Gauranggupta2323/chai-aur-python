3fa2466c9f8af875a7cf6618da61134853394c0e
Gauranggupta2323 <gauranggupta2310@gmail.com>
2025-06-23 06:57:28 +0530

    Update 01_scope.py

--- 06_scopes/01_scope.py
+++ 06_scopes/01_scope.py
@@ -1,10 +1,10 @@
 username = "chaiaurcode"
 
 def func():
-    # username = "chai"
-    print(username)
+     username = "chai"
+    print(username)        # chaiaurcode aaega kyuki vo global hi
 
-print(username)
+print(username)   # ismai chai aaega kyuki niche function call kr diya 
 func()
 
 
@@ -13,11 +13,11 @@ x = 99
 #     z = x + y
 #     return z
 
-# result = func2(1)
-# print(result)
+# result = func2(1)      // isko result maii store kiya
+# print(result)       // x ko 99 lenge  aur y ko 1
 
 # def func3():
-#     global x
+#     global x       // avoid krna ise use krne ka
 #     x = 12
     
 # func3()
@@ -29,20 +29,20 @@ def f1():
     x = 88
     def f2():
         print(x)
-    return f2
+    return f2         // f2 ka pura definition bheja hai return ke through  // bina f1 ke bulaye 99 aaega
 myResult = f1()
-myResult()
+myResult()           // 88
 
 
 def chaicoder(num):
-    def actual(x):
+    def actual(x):              // Closure or  factor function
         return x ** num
     return actual
 
 
 
-f = chaicoder(2)
-g = chaicoder(3)
+f = chaicoder(2)    // ye square calculate karega 
+g = chaicoder(3)    // ye cube calculate karega 
 
 print(f(3))
 print(g(3))
